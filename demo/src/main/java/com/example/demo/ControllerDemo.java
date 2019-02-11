631280631fbcb9f4b378d9551d8053dce4d816fe
NachoMC <44577104+NachoMC@users.noreply.github.com>
2019-02-11 14:25:35 +0100

    html's navigability

--- demo/src/main/java/com/example/demo/ControllerDemo.java
+++ demo/src/main/java/com/example/demo/ControllerDemo.java
@@ -1,12 +1,37 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerDemo {
+	
 	@GetMapping("/")
 	public String home(Model model) {
+		model.addAttribute("name", "Inicio");
 		return "Inicio";
 	}
+	
+	@GetMapping("/tema")
+	public String tema(Model model) {
+		return "Temas";
+	}
+
+	@GetMapping("/cita")
+	public String cita(Model model) {
+		return "Citas";
+	}
+
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "LogIn";
+	}
+
+	@GetMapping("/histograma")
+	public String histograma(Model model) {
+		return "Histograma";
+	}
+
 }
