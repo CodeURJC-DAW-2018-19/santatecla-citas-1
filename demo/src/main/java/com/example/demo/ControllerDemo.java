f8f53cd2c6e08b50cf238ae760adc61e20fdba61
NachoMC <44577104+NachoMC@users.noreply.github.com>
2019-02-11 12:21:43 +0100

    mustache

--- demo/src/main/java/com/example/demo/ControllerDemo.java
+++ demo/src/main/java/com/example/demo/ControllerDemo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ControllerDemo {
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
 		return "Inicio";
 	}
 }
