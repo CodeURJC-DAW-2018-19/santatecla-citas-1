47a25488053dc35ad0021f5edfcd5df376aab2e8
David Cuenca <davidcuencaortega@gmail.com>
2019-02-16 10:27:49 +0100

    DB relation of theme and quote

--- demo/src/main/java/com/DatabaseInitializer.java
+++ demo/src/main/java/com/DatabaseInitializer.java
@@ -24,23 +24,38 @@ public class DatabaseInitializer {
 	@PostConstruct
 	public void init() {
 
-		// Sample quotes
+        // Quotes
+        Quote quote1 = new Quote("Cita 1","Paco","MilYUnanochesConNacho");
+        Quote quote2 = new Quote("Cita 2","Jones","MilYUnanochesConNacho");
+        Quote quote3 = new Quote("Cita 3","Pablo","MilYUnanochesConNacho");
+        Quote quote4 = new Quote("Cita 4","Tyrion","MilYUnanochesConNacho");
+        Quote quote5 = new Quote("Cita 5","Daenerys","MilYUnanochesConNacho");
 
-		quoteRepository.save(new Quote("Cita 1","Paco","MilYUnanochesConNacho"));
-        quoteRepository.save(new Quote("Cita 2","Jones","MilYUnanochesConNacho"));
-        quoteRepository.save(new Quote("Cita 3","Pablo","MilYUnanochesConNacho"));
-		quoteRepository.save(new Quote("Cita 4","Tyrion","MilYUnanochesConNacho"));
-        quoteRepository.save(new Quote("Cita 5","Daenerys","MilYUnanochesConNacho"));
-        
-        // Sample themes
+		// Save quotes
+		quoteRepository.save(quote1);
+        quoteRepository.save(quote2);
+        quoteRepository.save(quote3);
+		quoteRepository.save(quote4);
+        quoteRepository.save(quote5);
 
-		themeRepository.save(new Theme("Amor"));
-        themeRepository.save(new Theme("Odio"));
-        themeRepository.save(new Theme("Vida"));
-        themeRepository.save(new Theme("Democracia"));
+        //Themes
+        Theme theme1 = new Theme("Amor");
+        Theme theme2 = new Theme("Odio");
+        Theme theme3 = new Theme("Vida");
+        Theme theme4 = new Theme("Democracia");
+
+        theme1.getQuotes().add(quote1);
+        theme1.getQuotes().add(quote2);
+        theme2.getQuotes().add(quote3);
+        theme2.getQuotes().add(quote4);
         
+        // Save themes
+		themeRepository.save(theme1);
+        themeRepository.save(theme2);
+        themeRepository.save(theme3);
+        themeRepository.save(theme4);
+
         // Sample users
-
 		userRepository.save(new User("user", "pass", "ROLE_USER"));
 		userRepository.save(new User("admin", "pass", "ROLE_USER", "ROLE_ADMIN"));
 
--- demo/src/main/java/com/theme/Theme.java
+++ demo/src/main/java/com/theme/Theme.java
@@ -1,9 +1,15 @@
 package com.theme;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.quote.*;
 
 @Entity
 public class Theme{
@@ -14,10 +20,18 @@ public class Theme{
     
     private String name;
 
+    @OneToMany
+    private List<Quote> quotes;
+
     public Theme(){}
 
     public Theme(String name){
         this.name = name;
+        this.quotes = new ArrayList<Quote>();
+    }
+
+    public List<Quote> getQuotes(){
+        return this.quotes;
     }
 
     public String getName(){
