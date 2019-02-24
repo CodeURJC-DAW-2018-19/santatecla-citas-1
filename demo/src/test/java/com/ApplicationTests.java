0708637d7eef3d28eca9508d4b11f18b95a53221
alexibro <alexibro2@gmail.com>
2019-02-24 19:09:35 +0100

    Tests added

--- demo/src/test/java/com/ApplicationTests.java
+++ demo/src/test/java/com/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import com.quote.*;
 import com.theme.*;
@@ -30,6 +31,50 @@ public class ApplicationTests {
 	public void setUp() {}
 
 	@Test
+	public void themeServiceAddTheme(){
+		Theme theme1 = new Theme("theme1");
+		themeService.save(theme1);
+		assertTrue(themeService.findByName("theme1").contains(theme1));
+	}
+
+	@Test
+	public void quoteServiceAddTheme(){
+		Quote quote = new Quote("test", "test", "test");
+		quoteService.save(quote);
+		assertTrue(quoteService.findByName("test").contains(quote));
+	}
+
+	@Test
+	public void themeServiceFindByName(){
+		Theme theme1 = new Theme("theme11");
+		Theme theme2 = new Theme("theme12");
+		Theme theme3 = new Theme("theme");
+		Theme theme4 = new Theme("test");
+		
+		themeService.save(theme1);
+		themeService.save(theme2);
+		themeService.save(theme3);
+		themeService.save(theme4);
+
+		assertEquals(2, themeService.findByName("theme1").size());
+	}
+
+	@Test
+	public void quoteServiceFindByName(){
+		Quote quote1 = new Quote("quote11", "test", "test");
+		Quote quote2 = new Quote("quote12", "test", "test");
+		Quote quote3 = new Quote("quote", "test", "test");
+		Quote quote4 = new Quote("test", "test", "test");
+		
+		quoteService.save(quote1);
+		quoteService.save(quote2);
+		quoteService.save(quote3);
+		quoteService.save(quote4);
+
+		assertEquals(2, quoteService.findByName("quote1").size());
+	}
+
+	/*@Test
 	public void noRepeatedElements() {
 
 		// Falla porque controlamos que no haya repetidos en el WebController
@@ -70,9 +115,5 @@ public class ApplicationTests {
 		assertEquals(1, theme1.getTexts().size());
 
 
-	}
-
-
-
-
+	}*/
 }
