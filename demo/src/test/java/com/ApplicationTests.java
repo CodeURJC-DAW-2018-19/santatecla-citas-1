38eafe19fe0cc4ea08cc8c50b08b7ede8223c1f3
alexibro <alexibro2@gmail.com>
2019-02-24 19:19:55 +0100

    Tests removed

--- demo/src/test/java/com/ApplicationTests.java
+++ demo/src/test/java/com/ApplicationTests.java
@@ -74,46 +74,4 @@ public class ApplicationTests {
 		assertEquals(2, quoteService.findByName("quote1").size());
 	}
 
-	/*@Test
-	public void noRepeatedElements() {
-
-		// Falla porque controlamos que no haya repetidos en el WebController
-
-		// Repeated themes
-		Theme theme1 = new Theme("theme1");
-		themeService.save(theme1);
-		themeService.save(new Theme(theme1.getName()));
-		assertEquals(1, themeService.findByName(theme1.getName()).size());
-
-		// Repeated quotes
-		Quote quote1 = new Quote("quote1", "-", "-");
-		quoteService.save(quote1);
-		quoteService.save(new Quote(quote1.getName(), quote1.getAuthor(), quote1.getBook()));
-		assertEquals(1, quoteService.findByName(quote1.getName()).size());
-		
-		// Repeated users
-		User user1 = new User("user1", "p");
-		userService.save(user1);
-		userService.save(new User(user1.getName(), user1.getPasswordHash()));
-		int n = 0;
-		for(User u : userService.findAllUsers()) {
-			if (u.getName().equals(user1.getName())) {
-				n++;
-			}
-		}
-		assertEquals(1, n);
-
-		// Repeated quotes from a theme
-		theme1.getQuotes().add(quote1);
-		theme1.getQuotes().add(quote1);
-		assertEquals(1, theme1.getQuotes().size());
-		
-		// Repeated texts from a theme
-		Text text1 = new Text("text1"); 
-		theme1.getTexts().add(text1);
-		theme1.getTexts().add(text1);
-		assertEquals(1, theme1.getTexts().size());
-
-
-	}*/
 }
