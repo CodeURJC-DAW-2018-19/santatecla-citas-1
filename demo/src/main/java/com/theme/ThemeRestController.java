956638555735e35beaecf5dda0471689c0642ccf
JesusBorrero <shyfu98@gmail.com>
2019-02-27 20:00:51 +0100

    Show Themes In API Rest Include

---
+++ demo/src/main/java/com/theme/ThemeRestController.java
@@ -0,0 +1,24 @@
+package com.theme;
+
+import java.util.List;
+
+import com.quote.QuoteService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ThemeRestController{
+    @Autowired
+	protected QuoteService quoteService;
+
+	@Autowired
+    protected ThemeService themeService;
+    
+    @GetMapping(value="/themes/")
+    public List<Theme> themes(){
+        return this.themeService.findAll();
+    }
+    
+}
---
+++ demo/src/main/resources/static/assets/js/RestScript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $.ajax({
+        url:"https://localhost:8443/themes/",
+        dataType: 'json'
+    }).done(function(data) {
+        for(var i=0; i<data.length; i++){
+            $("#home-themes").append(
+                "<div class=\"col-md-6\"><div class=\"card\"><div class=\"card-body\"><a href=\"/theme/"+data[i].id+"\" class=\"card-title\">"+data[i].name+"</a>"+
+                "<a title=\"Borrar\" id=\"delete\"onclick=\"confirmAlert('¿Está seguro de que quiere borrar este tema?','/theme/deleteTheme/"+data[i].id+"')\""+
+                "class=\"btn btn-primary btn-sm active pull-right\" role=\"button\" aria-pressed=\"true\"><i class=\"material-icons\">delete_outline</i></a>"+
+                "</div></div></div>");
+        }
+    });
+});
+   
+   
