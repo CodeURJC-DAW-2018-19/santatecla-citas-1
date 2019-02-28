1c57d415610404f9b6392db6e69fd5ea9f495c42
Alejandro Delgado Peribáñez <alexdelgadoperibanez@users.noreply.github.com>
2019-02-28 17:19:09 +0100

    Methods login and register && update Application

--- demo/src/main/java/com/user/UserRestController.java
+++ demo/src/main/java/com/user/UserRestController.java
@@ -1,8 +1,14 @@
 package com.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/user")
@@ -13,4 +19,37 @@ public class UserRestController{
 	
 	@Autowired
 	private UserService userService;
+
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public ResponseEntity<User> login() {
+		if (userComponent.getLoggedUser() != null){
+			return new ResponseEntity<>(userComponent.getLoggedUser(), HttpStatus.OK);
+		}
+		else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public ResponseEntity<User> register(@RequestBody User newUser) {
+		if(newUser == null){
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+		User findUser = userService.findOne(id);
+
+		//If the user is already sign in or already exists
+		if((userComponent.getLoggedUser() != null || findUser != null) && !userComponent.isAdmin(){
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		}
+
+		User user = new User();
+		user.setName(newUser.getName());
+		user.setPasswordHash(newUser.getPasswordHash());
+
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+	
+	
 }
