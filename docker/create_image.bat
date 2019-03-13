ac8eb79748cab8c05305050555f88f15f6c13658
NachoMC <44577104+NachoMC@users.noreply.github.com>
2019-03-13 13:49:23 +0100

    port 8080 & create_image script

--- docker/create_image.bat
+++ docker/create_image.bat
@@ -1,7 +1,19 @@
 cd ../app
+
+:: Create jar
 docker run -it --rm -v "%cd%":/usr/src/project -w /usr/src/project maven:alpine mvn package
+
+:: Copy jar and images
 copy target\app-0.0.1-SNAPSHOT.jar ..\docker\build\
 cd ..
 copy themes-images docker\build\themes-images\
 cd docker
+
+:: Delete containers and images
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
+docker rmi app
+
+:: Build app image
 docker build -t app .
+
+docker-compose up
