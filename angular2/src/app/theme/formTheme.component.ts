20ceb7511df1c5472a996ff61b895f40081906f3
David <davidcuencaortega@gmail.com>
2019-03-26 10:15:11 +0100

    AddTheme
    Co-Authored-By: Alejandro Delgado Peribáñez <alexdelgadoperibanez@users.noreply.github.com>
    Co-Authored-By: Alejandro Quesada <alexibro@users.noreply.github.com>

--- angular2/src/app/theme/formTheme.component.ts
+++ angular2/src/app/theme/formTheme.component.ts
@@ -1,4 +1,7 @@
+import { ThemeService } from './theme.service';
 import { Component, OnInit } from '@angular/core';
+import { Theme } from './theme.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './formTheme.component.html',
@@ -7,4 +10,38 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 
-export class FormThemeComponent {}
+export class FormThemeComponent {
+
+  newBook: boolean;
+  theme: Theme;
+
+    constructor(private _router: Router, activatedRoute: ActivatedRoute, private service: ThemeService) {
+        const id = activatedRoute.snapshot.params['id'];
+        if (id) {
+          this.service.getTheme(id)
+          .subscribe((data: Theme) => this.theme = {
+            id: data['id'],
+            name: data['name'],
+            quotes: data['quotes']
+          }
+        );
+            this.newBook = false;
+        } else {
+            this.theme = { name: '', id: null, quotes: []};
+            this.newBook = true;
+        }
+    }
+
+    cancel() {
+        window.history.back();
+    }
+
+    save() {
+        this.service.saveTheme(this.theme).subscribe(
+            _ => {},
+            (error: Error) => console.error('Error creating new book: ' + error),
+        );
+        window.history.back();
+    }
+
+}
