b87bd7bee542ad265a6971cd0a98f03fece79ae4
NachoMC <44577104+NachoMC@users.noreply.github.com>
2019-03-26 16:09:28 +0100

    login as a dialog

--- angular2/src/app/app.routing.ts
+++ angular2/src/app/app.routing.ts
@@ -1,9 +1,7 @@
-import { AppComponent } from './app.component';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ElementListComponent } from './element-list.component';
 import { ThemeComponent } from './theme/theme.component';
 import { QuoteComponent } from './quote/quote.component';
-import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register/register.component';
 import { FormQuoteComponent } from './quote/formQuote.component';
 import { FormThemeComponent } from './theme/formTheme.component';
@@ -15,7 +13,6 @@ const appRoutes = [
   { path: 'quote/:id', component: QuoteComponent  },
   { path: 'formQuote', component: FormQuoteComponent  },
   { path: 'formTheme', component: FormThemeComponent  },
-  { path: 'login', component: LoginComponent  },
   { path: 'register', component: RegisterComponent  },
   { path: 'histogram', component: HistogramComponent  }
 ];
