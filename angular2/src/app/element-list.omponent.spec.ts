e73166b2a52ced2d7feac64a9a30964e2d2cf889
alexibro <alexibro2@gmail.com>
2019-04-07 23:20:16 +0200

    Update element-list.omponent.spec.ts

--- angular2/src/app/element-list.omponent.spec.ts
+++ angular2/src/app/element-list.omponent.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed, async, ComponentFixture, inject, tick, fakeAsync, RouterTestingModule  } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture, inject, tick, fakeAsync} from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { By } from '@angular/platform-browser';
